aeed8352d9c4ed36c5acfc44d66ff82dadd93401
anilbhajgotar <bhajgotarab@gmail.com>
2021-09-13 18:50:09 +0530

    add apache,mysql,adminer in yml file using d-file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM php:7.4-cli
-COPY . /usr/src/myapp
-WORKDIR /usr/src/myapp
-CMD [ "php", "./index.php" ]
+FROM php:7.4-apache
+# COPY . /usr/src/myapp
+# WORKDIR /usr/src/myapp
+# CMD [ "php", "./index.php" ]
+RUN docker-php-ext-install mysqli
--- app/index.php
+++ app/index.php
@@ -2,4 +2,17 @@
     echo 'Hello World From Hiren ';
     echo '<br>';
     echo 'hehehehe';
+
+$conn = new mysqli('db', 'root', 'password', 'dockertest1');
+$sql = "SELECT * FROM docker_table1";
+
+if($res = $conn->query($sql)) {
+    while($data = $res->fetch_object()) {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+        // exit;
+    }
+}
+
 ?>
